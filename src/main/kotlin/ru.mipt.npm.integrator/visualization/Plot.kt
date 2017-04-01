b389078e7a6d6b105b77bbc0b68bf466491c273e
Alexander Nozik <altavir@gmail.com>
2017-04-01 09:12:30 +0300

    ????????????? ????? ? ?????????????

--- src/main/kotlin/ru.mipt.npm.integrator/visualization/Plot.kt
+++ src/main/kotlin/ru.mipt.npm.integrator/visualization/Plot.kt
@@ -5,18 +5,19 @@ import javafx.collections.FXCollections
 import javafx.scene.chart.NumberAxis
 import javafx.scene.chart.XYChart
 import ru.mipt.npm.integrator.badFunc
+import ru.mipt.npm.integrator.sinFunc
 import tornadofx.*
 
 /**
  * Created by darksnake on 29-Mar-17.
  */
 class Plot : View("My View") {
-    val theData = FXCollections.observableArrayList<XYChart.Series<Number,Number>>();
+    val theData = FXCollections.observableArrayList<XYChart.Series<Number, Number>>();
 
 
     override val root = borderpane {
         center {
-            linechart("function demo",NumberAxis(),NumberAxis()){
+            linechart("function demo", NumberAxis(), NumberAxis()) {
                 id = "plot";
                 data = theData
                 createSymbols = false
@@ -25,11 +26,13 @@ class Plot : View("My View") {
     }
 
     init {
-        displayFunction(badFunc,0.0, Math.PI,"test")
+        displayFunction(badFunc, 0.0, Math.PI, "bad")
+        displayFunction(sinFunc, 0.0, Math.PI, "sin")
+        displayFunction({ x -> x * x }, 0.0, Math.PI, "parabola")
     }
 
-    fun displayFunction(function: (Double) -> Double, a: Double, b: Double, label:String = "", numPoints: Int = 200){
-        val series = XYChart.Series<Number,Number>();
+    fun displayFunction(function: (Double) -> Double, a: Double, b: Double, label: String = "", numPoints: Int = 200) {
+        val series = XYChart.Series<Number, Number>();
         series.name = label;
 
 
@@ -37,7 +40,7 @@ class Plot : View("My View") {
 
         (0..numPoints)
                 .map { a + it * step }
-                .forEach { series.data.add(XYChart.Data(it,function(it))) }
+                .forEach { series.data.add(XYChart.Data(it, function(it))) }
         Platform.runLater { theData.add(series) }
     }
 }
--- src/main/kotlin/ru.mipt.npm.integrator/visualization/Viewer.kt
+++ src/main/kotlin/ru.mipt.npm.integrator/visualization/Viewer.kt
@@ -1,9 +1,15 @@
 package ru.mipt.npm.integrator.visualization
 
+import javafx.stage.Stage
 import tornadofx.*
 
 /**
  * Created by darksnake on 29-Mar-17.
  */
 class Viewer: App(Plot::class) {
+
+    override fun start(stage: Stage) {
+        super.start(stage)
+        stage.isFullScreen = true
+    }
 }
